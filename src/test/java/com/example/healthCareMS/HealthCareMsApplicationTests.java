c59fbd4c1c09464a742ea52a27fcdd9f7e812748
Anushka <anushkasahan200439@gmail.com>
2025-07-28 17:48:19 +0530

    Initial commit

---
+++ src/test/java/com/example/healthCareMS/HealthCareMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthCareMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
